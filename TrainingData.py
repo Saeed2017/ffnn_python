8453317a2932f8e262294e6916f97e2ba734280f
Soham Gadgil <sohamudaygadgil@gmail.com>
2016-12-10 17:01:32 -0500

    Files Deleted

--- TrainingData.py
+++ TrainingData.py
@@ -1,49 +1,103 @@
 import math
 import numpy
+import matplotlib.pyplot as pyplot
+
+import sys
 def TrainingData_Generation(opt):
    
     if opt == 1:
-        TrainingData_input = [];
-        TrainingData_output = [];
-        for x in range(0, 60, 60/1000):
-            TrainingData_input.append(x); 
-            TrainingData_output.append(math.sin(x) * math.sin(x));
-    
-        TrainingData_input = numpy.zeros((1, 91));
-        TrainingData_input[0, 15:25] = 1;
-        TrainingData_input[0, 38:68] = 1;
-        TrainingData_input[0, 73:83] = 1;
+        train_in = numpy.arange(0, 60, 60/1000.0)   # 0:60/1000:60
+        train_out = numpy.sin(train_in)
+        input_length = len(train_in)
+        
+        train_in = numpy.ones(input_length)
 
-        TrainingData_output = numpy.zeros((1,91));
-        TrainingData_output[0, 16:26] = 1;
-        TrainingData_output[0, 39:69] = 1;
-        TrainingData_output[0, 74:84] = 1;
-
-        TrainingData_input = [TrainingData_input, TrainingData_output];
-        TrainingData_output = 1 - TrainingData_output;
-
-        TrainingData_input = [];
-        TrainingData_output = [];
-
-        for x in range(0, 120, 120/1000):
-            TrainingData_input.append(x); 
-            TrainingData_output.append(math.sin(x));
-
-        input_length = len(TrainingData_input);
-        TrainingData_input = numpy.ones((1, input_length));
 
     elif opt == 2:
-        TrainingData_output = [];
-
-        for aa in range(0, 120, 120/1000):
-            TrainingData_output.append(math.sin(aa));
-
-        TrainingData_input = numpy.ones((1, len(TrainingData_output)));
-
+        aa = numpy.arange(0, 120, 120/1000.0)   # 0:120/1000:120
+        
+        train_out = numpy.sin(aa)
+        train_in = numpy.ones(len(train_out))
     elif opt == 3:
-        bb = [];
-        for aa in range(0, 120, 120/1000):
-            bb = math.sin(aa);
+        aa = numpy.arange(0, 120, 120/1000.0)   # 0:120/1000:120
+        bb = numpy.sin(aa)
+        train_in  = numpy.zeros(50)
+        train_out = numpy.zeros(50)
+        train_in  = numpy.append(train_in, numpy.ones(200))
+        train_out = numpy.append(train_out, bb[0:200])  # bb(1:200)
+        train_in  = numpy.append(train_in, numpy.zeros(60))
+        train_out = numpy.append(train_out, numpy.zeros(60))
+        train_in  = numpy.append(train_in, numpy.ones(100))
+        train_out = numpy.append(train_out, bb[0:100])  # bb(1:100)
+        train_in  = numpy.append(train_in, numpy.zeros(30))
+        train_out = numpy.append(train_out, numpy.zeros(30))
+
+        train_in  = numpy.stack((train_in, numpy.append(0, train_in[:len(train_in)-1]), numpy.append([0, 0], train_in[:len(train_in)-2])))
+        train_out = numpy.append(0, train_out[:len(train_out)-1])
+    elif opt == 4:
+        aa = numpy.concatenate((numpy.zeros(10), numpy.ones(10), numpy.zeros(10), numpy.ones(10), numpy.zeros(10), numpy.ones(10)))
+        bb = numpy.concatenate((aa, aa, aa, aa, aa, aa, aa, aa, aa, aa, aa, aa))
+        train_in = numpy.concatenate((numpy.zeros(90), numpy.ones(80), numpy.zeros(80), numpy.ones(60), numpy.zeros(100), numpy.ones(100), numpy.zeros(50)))
+        input_length = len(train_in)
+        
+        train_out = numpy.multiply(train_in, bb[:input_length])  # bb(1:input_length)
+        train_in = numpy.stack((train_in, numpy.append(0, train_in[:len(train_in)-1]), numpy.append([0, 0], train_in[:len(train_in)-2])))
+    elif opt == 5:
+        aa = numpy.arange(0, 50+0.1, 0.1)   #0:0.1:50
+        bb = numpy.sin(aa)
+        cc = [0] + bb
+        dd = numpy.subtract(numpy.power(bb, 2), numpy.multiply(cc, 2))
+                               
+        train_out = numpy.zeros(len(dd))
+        train_out[0] = dd[0]
+                               
+        for n in range(1, len(dd)):
+            train_out[n] = dd[n] - train_out[n-1]
+        train_in = numpy.stack((bb, cc))
+    elif opt == 6:
+        aa = numpy.multiply(numpy.random.random(1000), 2)
+        bb = numpy.multiply(numpy.random.random(1000), 2)
+        cc = numpy.multiply(aa, bb)
+        dd = numpy.random.random(1000)
+        
+        train_out = numpy.multiply(numpy.subtract(cc, numpy.power(numpy.subtract(aa, bb), 2)), dd)
+        train_in = numpy.stack((aa, bb, cc, dd))
+    elif opt == 7:
+        aa = numpy.arange(0, 1.5001, 0.001)  # 0:0.001:1.5
+        bb = numpy.arange(0, 1.5001, 0.001)  # 0:0.001:1.5
+        cc = numpy.multiply(aa, bb)
+        
+        train_out = numpy.subtract(cc, numpy.power(numpy.subtract(aa, bb), 2))
+        train_in = numpy.stack((aa, bb, cc))
+    elif opt == 8:
+        aa = numpy.arange(0, 60, 0.1) # 0:0.1:60
+        bb = numpy.sin(aa)
+        lg = len(aa)
+        bb_d = numpy.zeros(lg)
+        for n in numpy.arange(1, lg):
+            bb_d[n] = bb[n] - bb[n-1]
+            
+        train_in = numpy.stack((bb[2:len(bb)-1], bb_d[2:len(bb_d)-1]))
+        train_out = bb_d[3:]
+    else:           # Default sin() waveform
+        aa = numpy.arange(0, 120+120/1000.0, 120/1000.0)
+        
+        train_out = numpy.sin(aa)
+        train_in = numpy.ones(len(train_out))
+
+    # Graph the Training Data Inputs and Outputs
+    pyplot.figure()
+    pyplot.title("Training Data Output")
+    pyplot.plot(train_out)
+    pyplot.show()
+    
+    return [train_in, train_out]
+
+
+if __name__ == '__main__':
+    for i in range(1, 10):
+        TrainingData_Generation(i)
+    sys.exit
 
         
     #more changes to be made
